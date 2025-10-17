38329688f1f9e4b321150c8467df641b9dc86596
kht <gudxor1541@naver.com>
2025-10-17 12:11:44 +0900

    13_redux_toolkit 종료

--- 13_redux_toolkit/src/App.js
+++ 13_redux_toolkit/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {decrement, increment, reset} from "./features/counterSlice";
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const number = useSelector(state => state.counter.number);
+
+    const dispatch = useDispatch();
+
+
+    return (
+        <div>
+            <h1>현재 상태 number: {number}</h1>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(reset())}>reset</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ 13_redux_toolkit/src/features/counterSlice.js
@@ -0,0 +1,30 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const counterSlice =
+    createSlice({
+        name: 'counter',
+        initialState: {
+            number: 0,
+        },
+        reducers: {
+            increment: state => {
+                state.number += 1;
+            },
+            decrement: state => {
+                state.number -= 1;
+            },
+            reset: state => {
+                state.number = 0;
+            },
+        }
+    });
+
+
+export default counterSlice.reducer;
+
+export const {
+    increment,
+    decrement,
+    reset
+} = counterSlice.actions;
+
